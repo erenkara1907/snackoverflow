05aea4d447cd0ee89b9c276825df3e5915619b58
erenkara1907 <erenkaraaa47@gmail.com>
2022-11-22 11:08:05 +0300

    Initial Commit

---
+++ SnackOverFlow/SnackOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowApp.swift
+//  SnackOverFlow
+//
+//  Created by Eren Kara on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
